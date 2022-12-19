2e9fb4f0f1609424321d81a68e9c46fe175f21e8
Developer <indira@MBP-Indira-2.Home>
2022-12-20 00:33:37 +0600

    Dark_Light

---
+++ Dark_Light/Profile/LikedPostsViewController.swift
@@ -0,0 +1,125 @@
+//
+//  LikedPostsViewController.swift
+//  Navigation_2
+//
+//  Created by Developer on 15.11.2022.
+//
+
+import UIKit
+import SnapKit
+
+class LikedPostsViewController: UIViewController {
+    
+    let coreManager = CoreDataManager.shared
+    
+    private enum CellReuseIdentifiers: String {
+        case likedPosts
+    }
+    
+    private lazy var likedPostsLabel: UILabel = {
+        let likedPostsLabel = UILabel()
+        likedPostsLabel.textColor = .blue
+        likedPostsLabel.text = "double_click".localized
+        likedPostsLabel.font = UIFont.boldSystemFont(ofSize: 15)
+        return likedPostsLabel
+    }()
+
+    
+    private lazy var likedPostsTableView: UITableView = {
+        let likedPostsTableView = UITableView(frame: .zero, style: .grouped)
+        likedPostsTableView.register(LikedPostsTableViewCell.self, forCellReuseIdentifier: CellReuseIdentifiers.likedPosts.rawValue)
+        likedPostsTableView.delegate = self
+        likedPostsTableView.dataSource = self
+        return likedPostsTableView
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupView()
+        
+        navigationItems()
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(self.refreshTableView), name: NSNotification.Name(rawValue: "newDataNotif"), object: nil)
+    }
+    
+    private func navigationItems() {
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "delete_all".localized, style: .plain, target: self, action: #selector(deleteAll))
+        navigationItem.rightBarButtonItem?.tintColor = .red
+    }
+    
+    @objc func refreshTableView() {
+        self.likedPostsTableView.reloadData()
+    }
+    
+    @objc func deleteAll() {
+        coreManager.deleteAll()
+        coreManager.posts = []
+        likedPostsTableView.reloadData()
+    }
+    
+    private func setupView() {
+        view.backgroundColor = .blackWhiteColor
+        title = "favourite_posts".localized
+        
+        view.addSubview(likedPostsLabel)
+        view.addSubview(likedPostsTableView)
+        
+        likedPostsLabel.snp.makeConstraints {
+            $0.top.equalToSuperview().offset(120)
+            $0.centerX.equalToSuperview()
+        }
+        
+        likedPostsTableView.snp.makeConstraints {
+            $0.left.right.bottom.equalToSuperview()
+            $0.top.equalTo(likedPostsLabel.snp.bottom).offset(16)
+        }
+    }
+}
+
+extension LikedPostsViewController: UITableViewDataSource, UITableViewDelegate {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return coreManager.posts.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(
+            withIdentifier: CellReuseIdentifiers.likedPosts.rawValue) as! LikedPostsTableViewCell
+        let post = coreManager.posts[indexPath.row]
+        cell.authorLabel.text = post.author
+        cell.descriptionLabel.text = post.descript
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let post = coreManager.posts[indexPath.row]
+        let tapRecognizer = TapGestureRecognizer(block: { [self] in
+            let alert = UIAlertController(title: "deleting_post".localized, message: "want_delete_post".localized, preferredStyle: UIAlertController.Style.alert)
+
+            alert.addAction(UIAlertAction(title: "cancel".localized, style: UIAlertAction.Style.default, handler: { _ in
+                            //Cancel Action
+                        }))
+            alert.addAction(UIAlertAction(title: "delete".localized,
+                                                      style: UIAlertAction.Style.destructive,
+                                                      handler: {(_: UIAlertAction!) in
+                            self.coreManager.deletePosts(post: post)
+                            tableView.reloadData()
+                        }))
+                        self.present(alert, animated: true, completion: nil)
+        })
+        tapRecognizer.numberOfTapsRequired = 2
+        view.addGestureRecognizer(tapRecognizer)
+    }
+
+
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return UITableView.automaticDimension
+    }
+}
